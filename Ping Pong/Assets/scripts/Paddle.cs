1c89bde5e022dbf9f612aec91d825f601a793646
ami <amit2rockpower@gmail.com>
2017-10-14 00:05:20 +0530

    Half completed

---
+++ Ping Pong/Assets/scripts/Paddle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour 
+{
+	public float paddleSpeed = 1;
+	public Vector3 playerPos;
+
+	void Update()
+	{
+		float yPos = transform.position.y + (Input.GetAxis("Vertical") * paddleSpeed);
+		playerPos = new Vector3(-18, Mathf.Clamp(yPos, -11, 11), 0);
+		transform.position = playerPos;
+	}
+}
