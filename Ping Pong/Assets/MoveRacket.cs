abe6297bd96e60d5518cf5a80cf8f55f7fd8a4da
ami <amit2rockpower@gmail.com>
2017-10-15 00:05:19 +0530

    Addition of script to movement  of
    Addition of script for the movement of the rackets

---
+++ Ping Pong/Assets/MoveRacket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour 
+{
+	public float speed=30;
+	void FixedUpdate()
+	{
+		float v = Input.GetAxisRaw ("Vertical");
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, v) * speed;
+
+	}
+}
