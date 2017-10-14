2581b84ed85e142c9bd34896b1a35d6c292278fd
ami <amit2rockpower@gmail.com>
2017-10-15 00:23:07 +0530

    Movement of rackets working separately

--- Ping Pong/Assets/MoveRacket.cs
+++ Ping Pong/Assets/MoveRacket.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class MoveRacket : MonoBehaviour 
 {
 	public float speed=30;
+	public string axis="Vertical";
+
 	void FixedUpdate()
 	{
-		float v = Input.GetAxisRaw ("Vertical");
+		float v = Input.GetAxisRaw (axis);
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, v) * speed;
 
 	}
