7d2e22f8601afb79d5db13c7425eea6e5cf04c96
ami <amit2rockpower@gmail.com>
2017-10-15 00:50:14 +0530

    60%ping pong
    Now its kinda pong

---
+++ Ping Pong/Assets/Ball.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public float speed = 30;
+
+	void Start() {
+		// Initial Velocity
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+}
