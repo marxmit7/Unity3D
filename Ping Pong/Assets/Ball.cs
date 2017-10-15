ce917a4b19fe1e13ddba07770fb6685a1f9df5b2
ami <amit2rockpower@gmail.com>
2017-10-15 23:25:34 +0530

    Completed this game
    This is a tutorial based game to learn basics of Collisions ,object, 2d
    , little scripting

--- Ping Pong/Assets/Ball.cs
+++ Ping Pong/Assets/Ball.cs
@@ -9,4 +9,46 @@ public class Ball : MonoBehaviour {
 		// Initial Velocity
 		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
 	}
+
+	float hitFactor( Vector2 ballPos, Vector2 racketPos,float racketHeight)
+	{
+		return (ballPos.y - racketPos.y) / racketHeight;
+	}
+	void OnCollisionEnter2D(Collision2D col) {
+		// Note: 'col' holds the collision information. If the
+		// Ball collided with a racket, then:
+		//   col.gameObject is the racket
+		//   col.transform.position is the racket's position
+		//   col.collider is the racket's collider
+
+		// Hit the left Racket?
+		if (col.gameObject.name == "RacketLeft") {
+			// Calculate hit Factor
+			float y = hitFactor(transform.position,
+				col.transform.position,
+				col.collider.bounds.size.y);
+
+			// Calculate direction, make length=1 via .normalized
+			Vector2 dir = new Vector2(1, y).normalized;
+
+			// Set Velocity with dir * speed
+			GetComponent<Rigidbody2D>().velocity = dir * speed;
+		}
+
+		// Hit the right Racket?
+		if (col.gameObject.name == "RacketRight") {
+			// Calculate hit Factor
+			float y = hitFactor(transform.position,
+				col.transform.position,
+				col.collider.bounds.size.y);
+
+			// Calculate direction, make length=1 via .normalized
+			Vector2 dir = new Vector2(-1, y).normalized;
+
+			// Set Velocity with dir * speed
+			GetComponent<Rigidbody2D>().velocity = dir * speed;
+		}
+	}
+
+
 }
